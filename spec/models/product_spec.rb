dbda535f4b2a879c70cb77273eda5ab6e923ef40
Stefan Talbot <satalbot@protonmail.com>
2022-12-29 23:09:53 -0500

    Add Product Tests
    + Added: Skeleton for Product Rspec tests.

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "should give an error if no name exists" do
+    end
+
+    it "should give an error if no price exists" do
+    end
+
+    it "should give an error if no quantity exists" do
+    end
+
+    it "should give an error if no category exists" do
+    end
+
+    it "should contain a name, price, qunatity, and category" do
+    end
+  end
+end
