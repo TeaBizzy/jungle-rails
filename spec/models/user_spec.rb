7df83c158c36d364bf92b10a96e3e937b4dbe407
Stefan Talbot <satalbot@protonmail.com>
2022-12-30 12:15:53 -0500

    Add Test for User Model
    + Added: Skeleton for tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    it "should give an error if e-mail is blank" do
+    end
+
+    it "should give an error if first name is blank" do
+    end
+
+    it "should give an error if last name is blank" do
+    end
+
+    it "should give an error if password is blank" do
+    end
+
+    it "should have a password with a length greater than 8" do
+    end
+
+    it "should have matching password and password confirmation fields" do
+    end
+
+    it "should have a unique e-mail" do
+    end
+  end
+end
