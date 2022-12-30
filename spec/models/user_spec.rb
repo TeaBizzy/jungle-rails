1b45b10dbaa4451d86d9ef85697655a9ee5831d3
Stefan Talbot <satalbot@protonmail.com>
2022-12-30 12:27:46 -0500

    Add Test for User Model
    + Added: Test should give an error if e-mail is blank.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it "should give an error if e-mail is blank" do
+      @user = User.new(first_name: "John", last_name: "Doe", email: nil, password: "password")
+      @user.valid?
+      expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
+      expect(@user.errors.full_messages.length).to eq(1)
     end
 
     it "should give an error if first name is blank" do
