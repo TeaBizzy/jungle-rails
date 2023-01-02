7bd82348184f43de968c3013f19aaffa9738fa49
Stefan Talbot <satalbot@protonmail.com>
2023-01-02 13:54:34 -0500

    Add Cypress To The Project

---
+++ config/initializers/cypress_rails.rb
@@ -0,0 +1,20 @@
+require 'database_cleaner/active_record'
+DatabaseCleaner.strategy = :truncation
+
+return unless Rails.env.test?
+
+CypressRails.hooks.before_server_start do
+  # Called once, before either the transaction or the server is started
+end
+
+CypressRails.hooks.after_transaction_start do
+  # Called after the transaction is started (at launch and after each reset)
+end
+
+CypressRails.hooks.after_state_reset do
+  # Triggered after `/cypress_rails_reset_state` is called
+end
+
+CypressRails.hooks.before_server_stop do
+  # Called once, at_exit
+end
